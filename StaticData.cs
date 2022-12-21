d53f3881bc6f639ace7a30204532f9f6d9975e8a
2Xpro-pop <bay.asanov@mail.ru>
2022-12-21 10:03:18 +0600

    Started use api

---
+++ StaticData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace AvaloniaClient;
+public static class StaticData
+{
+    public static string Jwt { get; set; }
+
+    public static RestClientOptions RestClientOptions
+    {
+        get;
+    } = new RestClientOptions("http://localhost:80")
+    {
+        ThrowOnAnyError= true,
+    };
+
+    public static RestClient RestClient { get; } = new RestClient(RestClientOptions); 
+}
--- ViewModels/LoginWindowViewModel.cs
+++ ViewModels/LoginWindowViewModel.cs
@@ -4,20 +4,40 @@ using System.Reactive;
 using System.Text;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
+using RestSharp;
 
 namespace AvaloniaClient.ViewModels;
 public class LoginWindowViewModel : ViewModelBase
 {
     [Reactive] public string Login { get; set; }
     [Reactive] public string Password { get; set; }
-
+    [Reactive] public string Status { get; set;}
     public ReactiveCommand<Unit, bool> LoginCommand { get; }
 
     public LoginWindowViewModel()
     {
-        LoginCommand = ReactiveCommand.Create(() =>
+        LoginCommand = ReactiveCommand.CreateFromTask(async () =>
         {
-            return true;
+            var rest = new RestRequest($"auth/login/{Login}/{Password}");
+            var token = await StaticData.RestClient.GetAsync(rest);
+
+            if (token.IsSuccessful)
+            {
+                StaticData.Jwt = token.Content;
+                return true;
+            }
+
+            return false;
+        });
+
+        LoginCommand.Subscribe(result =>
+        {
+            Status = result ? "" : "Incorrect login or password...";
+        });
+
+        LoginCommand.ThrownExceptions.Subscribe(exc =>
+        {
+            Status = "Something went wrong...";
         });
     }
 }
